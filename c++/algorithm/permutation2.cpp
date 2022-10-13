f831ae8f4a1cdfd432804ee0bd3e3f1272e26300
CtrlShift-Esc <jehovah0427@gmail.com>
2022-10-13 18:40:23 +0900

    add

---
+++ c++/algorithm/permutation2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	//5C2
+	vector<int> v = { 1, 2, 3, 4, 5 };
+	int n = v.size();
+	int r = 2;
+
+	do
+	{
+		for (int i = 0; i < r; i++)
+			cout << v[i] << " ";
+		cout << '\n';
+
+		reverse(v.begin() + r, v.end());
+
+	} while (next_permutation(v.begin(), v.end()));
+
+}
